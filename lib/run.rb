c52aafbad40864f55d685dbecee428d22e8e4673
Joseph Shaw <j.g.shaw95@gmail.com>
2018-04-21 12:26:17 +0100

    Fix some rubocop errors

--- lib/run.rb
+++ lib/run.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rubg'
 require 'socket'
 require_relative 'metric_extractor'
@@ -12,28 +14,27 @@ past_matches = {}
 
 rubg_client = RUBG.new(api_key: api_key)
 
-while true do
+loop do
   matches = {}
   metrics = []
 
-  players_response = rubg_client.players(shard: shard, query_params: {player_names: player_names})
+  players_response = rubg_client.players(shard: shard, query_params: { player_names: player_names })
   players_response.players.each do |player|
     latest_match_id = player.match_ids.first
-   
-    unless past_matches[player.name] == latest_match_id
-      if !matches.key?(latest_match_id)
-        latest_match = rubg_client.match(shard: shard, query_params: {match_id: latest_match_id})
-        matches[latest_match_id] = latest_match
-      else
-        latest_match = matches[latest_match_id]
-      end
-      past_matches[player.name]  = latest_match.match_id
 
-      # Process the latest match
-      participant = latest_match.participants.select { |participant| participant.player_id == player.player_id }.first
-      
-      metrics = MetricExtractor.extract(player, participant, latest_match)  
+    next if past_matches[player.name] == latest_match_id
+    if !matches.key?(latest_match_id)
+      latest_match = rubg_client.match(shard: shard, query_params: { match_id: latest_match_id })
+      matches[latest_match_id] = latest_match
+    else
+      latest_match = matches[latest_match_id]
     end
+    past_matches[player.name] = latest_match.match_id
+
+    # Process the latest match
+    participant = latest_match.participants.select { |participant| participant.player_id == player.player_id }.first
+
+    metrics = MetricExtractor.extract(player, participant, latest_match)
   end
 
   puts metrics
