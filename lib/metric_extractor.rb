40667f4a17b47b1ed79d1c3cc15a9739ed991874
Joseph Shaw <j.g.shaw95@gmail.com>
2018-04-24 13:53:52 +0100

    Start at vcr testing

--- lib/metric_extractor.rb
+++ lib/metric_extractor.rb
@@ -2,7 +2,10 @@ require 'rubg'
 
 class MetricExtractor
 
+  attr_accessor :latest_matches
+
   def initialize(api_key)
+    @latest_matches = {}
     @client = RUBG.new(api_key: api_key)
   end
 
@@ -10,35 +13,51 @@ class MetricExtractor
     @client.players(shard: shard, query_params: { player_names: player_names })
   end
 
-  def self.extract(_player, participant, match)
-    time = Time.parse(match.created).to_i
+  def extract(shard, player)
+    return [] if player.match_ids.first == latest_matches[player] 
+    
+    last_match = match(shard, player, 0)
+    
+    time = Time.parse(last_match.created).to_i
+    participant = last_match.participants.select { |participant| participant.player_id == player.player_id }.first
     metrics = []
     metrics << match_metric(participant, 'dbnos', participant.dbnos, time)
     metrics << match_metric(participant, 'kills', participant.kills, time)
     metrics << match_metric(participant, 'assists', participant.assists, time)
-    metrics << match_metric(participant, 'boosts', participant.boosts, time)
-    metrics << match_metric(participant, 'damage_dealt', participant.damage_dealt, time)
-    metrics << match_metric(participant, 'headshot_kills', participant.headshot_kills, time)
-    metrics << match_metric(participant, 'heals', participant.heals, time)
-    metrics << match_metric(participant, 'longest_kill', participant.longest_kill, time)
-    metrics << match_metric(participant, 'revives', participant.revives, time)
-    metrics << match_metric(participant, 'ride_distance', participant.ride_distance, time)
-    metrics << match_metric(participant, 'road_kills', participant.road_kills, time)
-    metrics << match_metric(participant, 'time_survived', participant.time_survived, time)
-    metrics << match_metric(participant, 'vehicle_destroys', participant.vehicle_destroys, time)
-    metrics << match_metric(participant, 'walk_distance', participant.walk_distance, time)
-
-    metrics << match_metric(participant, 'number_of_teams', match.rosters.length, time)
-
-    player_roster = match.rosters.find { |roster| roster.participants.member?(participant) }
-    metrics << match_metric(participant, 'rank', player_roster.rank, time)
-
     metrics
   end
 
+  # def self.extract(participant, match)
+    # time = Time.parse(match.created).to_i
+    # metrics = []
+    # metrics << match_metric(participant, 'dbnos', participant.dbnos, time)
+    # metrics << match_metric(participant, 'boosts', participant.boosts, time)
+    # metrics << match_metric(participant, 'damage_dealt', participant.damage_dealt, time)
+    # metrics << match_metric(participant, 'headshot_kills', participant.headshot_kills, time)
+    # metrics << match_metric(participant, 'heals', participant.heals, time)
+    # metrics << match_metric(participant, 'longest_kill', participant.longest_kill, time)
+    # metrics << match_metric(participant, 'revives', participant.revives, time)
+    # metrics << match_metric(participant, 'ride_distance', participant.ride_distance, time)
+    # metrics << match_metric(participant, 'road_kills', participant.road_kills, time)
+    # metrics << match_metric(participant, 'time_survived', participant.time_survived, time)
+    # metrics << match_metric(participant, 'vehicle_destroys', participant.vehicle_destroys, time)
+    # metrics << match_metric(participant, 'walk_distance', participant.walk_distance, time)
+
+    # metrics << match_metric(participant, 'number_of_teams', match.rosters.length, time)
+
+    # player_roster = match.rosters.find { |roster| roster.participants.member?(participant) }
+    # metrics << match_metric(participant, 'rank', player_roster.rank, time)
+
+    # metrics
+  # end
+
+  def match(shard, player, index)
+    @client.match(shard: shard, query_params: { match_id: player.match_ids[index] })
+  end
+  
   private
 
-  def self.match_metric(participant, stat_name, stat, time)
+  def match_metric(participant, stat_name, stat, time)
     "PUBG.#{participant.name}.matches.#{stat_name} #{stat} #{time}"
   end
 end
--- spec/metric_extractor_spec.rb
+++ spec/metric_extractor_spec.rb
@@ -3,34 +3,90 @@ require 'timecop'
 require 'vcr'
 
 describe MetricExtractor do
-  let (:player) { double }
-  let (:participant) { double.as_null_object }
-  let (:match) { double.as_null_object }
-  let (:rosters) { double.as_null_object }
-  let (:roster) { double.as_null_object }
 
+  subject { MetricExtractor.new ENV['PUBG_API_KEY'] }
 
   before do
     VCR.configure do |config|
       config.cassette_library_dir = "spec/cassettes"
       config.hook_into :webmock
+      config.filter_sensitive_data('<API_KEY>') { ENV['PUBG_API_KEY'] }
     end
     
-    # allow(participant).to receive(:name).and_return('shroud')
-    # allow(match).to receive(:created).and_return('2018-04-17T12:32:33+00:00')
+    VCR.use_cassette('shroud') do
+      @shroud = subject.players('pc-na', 'shroud').players.first
+    end
   end
 
-  context '#call_api' do 
-  
+  context '#players' do  
     it 'gets the correct players' do
-      VCR.use_cassette('player_api_call') do
-        extractor = MetricExtractor.new(ENV['PUBG_API_KEY'])
-        player_collection = extractor.players('pc-na', 'shroud,Wadu')
-        expect(player_collection.players[0].name).to eq 'shroud'
-        expect(player_collection.players[1].name).to eq 'Wadu'
+      VCR.use_cassette('shroud_Wadu') do
+        players = subject.players('pc-na', 'shroud,Wadu').players
+        expect(players[0].name).to eq 'shroud'
+        expect(players[1].name).to eq 'Wadu'
+        expect(players.length).to eq 2
+      end
+    end
+  end
+
+  context '#extract' do     
+
+    before do
+      VCR.use_cassette('shroud_match_0') do
+        @first_match = subject.match('pc-na', @shroud, 0)
+      end
+      VCR.use_cassette('shroud_match_1') do
+        @second_match = subject.match('pc-na', @shroud, 1)
       end
     end
 
+    context 'the latest match has not changed' do
+      before do
+        subject.latest_matches = { @shroud => @first_match.match_id }
+      end
+
+      it 'returns no metrics' do
+        expect(subject.extract('pc-na', @shroud)).to be_empty 
+      end
+    end
+
+    context 'a new match has been played' do
+      before do
+        @seconds_ma
+        subject.latest_matches = { @shroud => @second_match.match_id }
+        VCR.use_cassette('shroud_match_0') do
+          @metrics = subject.extract('pc-na', @shroud)
+        end
+      end
+     
+      it 'returns the dbnos metric' do
+          expect(@metrics).to include('PUBG.shroud.matches.dbnos 3 1524544931')
+      end
+      
+      it 'returns the kills metric' do
+          expect(@metrics).to include('PUBG.shroud.matches.kills 5 1524544931')
+      end
+      
+      it 'returns the assists metric' do
+          expect(@metrics).to include('PUBG.shroud.matches.assists 0 1524544931')
+      end
+    end
+  end
+  
+  context '#match' do
+    it 'can get the first match' do
+      VCR.use_cassette('shroud_match_0') do
+        match = subject.match('pc-na', @shroud, 0)
+        expect(match.match_id).to eq "aa089c48-b7e0-4ba9-8e57-f27e195012a1"
+      end
+    end
+
+    it 'can get the second match' do
+      VCR.use_cassette('shroud_match_1') do
+        match = subject.match('pc-na', @shroud, 1)
+        expect(match.match_id).to eq "2997f2b8-c262-4948-a28c-432f8121a5e6"
+      end
+    end
   end
 
   # context '#extract' do
