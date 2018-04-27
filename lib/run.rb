7f9925ca1fb78c6ae1cca53f5a22cf712d1fd3d8
Joseph Shaw <j.g.shaw95@gmail.com>
2018-04-27 18:11:55 +0100

    rescue all errors

--- lib/run.rb
+++ lib/run.rb
@@ -11,18 +11,25 @@ player_names = ARGV[4].dup
 extractor = MetricExtractor.new(api_key)
 
 loop do
-  metrics = []
+  begin
+    metrics = []
 
-  players = extractor.players(shard, player_names)
-  players.each do |player|
-    metrics.concat extractor.extract(shard, player)
+    players = extractor.players(shard, player_names)
+    players.each do |player|
+      metrics.concat extractor.extract(shard, player)
+    end
+
+    puts metrics
+
+    socket = TCPSocket.new(graphite_ip, graphite_port)
+    metrics.each do |metric|
+      socket.puts metric
+    end
+    socket.close
+  rescue StandardError => e
+    puts e
   end
 
-  socket = TCPSocket.new(graphite_ip, graphite_port)
-  metrics.each do |metric|
-    socket.puts metric
-  end
-  socket.close
 
   sleep 60
 end
